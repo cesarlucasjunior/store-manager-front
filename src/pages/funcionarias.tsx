914109a5829754f8da21faece4ce0b1c12c03b11
César Lucas Júnior <cesarlucasjunior93@gmail.com>
2024-02-19 21:15:37 -0300

    Criando os arquivos para tela de funcionarias

--- src/pages/funcionarias.tsx
+++ src/pages/funcionarias.tsx
@@ -1,9 +1,28 @@
 import Layout from "@/components/template/Layout";
+import { Table, TableHeader, TableBody, TableColumn, TableCell, TableRow } from "@nextui-org/table";
 
 export default function Funcionarias() {
     return (
       <Layout titulo="Funcionárias" subtitulo="Gerenciando as Funcionárias.">
-          <h3>Aqui organizaremos os dados de todas as funcionárias.</h3>
+          <Table aria-label="Example static collection table" removeWrapper>
+            <TableHeader>
+              <TableColumn>NOME</TableColumn>
+              <TableColumn>CPF</TableColumn>
+              <TableColumn>DATA DE NASCIMENTO</TableColumn>
+              <TableColumn>ENDEREÇO</TableColumn>
+              <TableColumn>DATA DE CONTRATAÇÃO</TableColumn>
+              <TableColumn>ENDEREÇO</TableColumn>
+            </TableHeader>
+            <TableBody emptyContent={"No rows to display."}>
+              {[]}
+               
+              {/* <TableRow key="1">
+                <TableCell>Tony Reichert</TableCell>
+                <TableCell>CEO</TableCell>
+                <TableCell>Active</TableCell>
+              </TableRow> */}
+            </TableBody>
+          </Table>
       </Layout>
     );
 }
